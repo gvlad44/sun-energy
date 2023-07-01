e99ab518fe598941225bce1cd885182073b3d9db
gvlad44 <gont.vlad@hotmail.com>
2023-07-01 22:08:17 +0300

    small fixes

--- sun-energy/src/app/modules/dashboard/dashboard.component.ts
+++ sun-energy/src/app/modules/dashboard/dashboard.component.ts
@@ -21,24 +21,36 @@ const ELEMENT_DATA: Address[] = [
     id: 1,
     address: 'Soseaua Alexandriei 29',
     energy_rate: 0.25,
-    contract_start_date: dayjs().format('DD/MM/YYYY - HH:mm'),
-    contract_end_date: dayjs().format('DD/MM/YYYY - HH:mm'),
+    contract_start_date: dayjs('2022-04-04T16:00:00.000Z').format(
+      'DD/MM/YYYY - HH:mm'
+    ),
+    contract_end_date: dayjs('2023-04-04T16:00:00.000Z').format(
+      'DD/MM/YYYY - HH:mm'
+    ),
   },
   {
     position: 2,
     id: 2,
     address: 'Calea Dorobantilor 5',
     energy_rate: 0.25,
-    contract_start_date: dayjs().format('DD/MM/YYYY - HH:mm'),
-    contract_end_date: dayjs().format('DD/MM/YYYY - HH:mm'),
+    contract_start_date: dayjs('2022-02-14T12:30:00.000Z').format(
+      'DD/MM/YYYY - HH:mm'
+    ),
+    contract_end_date: dayjs('2023-02-14T12:30:00.000Z').format(
+      'DD/MM/YYYY - HH:mm'
+    ),
   },
   {
     position: 3,
     id: 3,
     address: 'Bulevardul Tineretului 25',
     energy_rate: 0.28,
-    contract_start_date: dayjs().format('DD/MM/YYYY - HH:mm'),
-    contract_end_date: dayjs().format('DD/MM/YYYY - HH:mm'),
+    contract_start_date: dayjs('2022-05-09T10:00:00.000Z').format(
+      'DD/MM/YYYY - HH:mm'
+    ),
+    contract_end_date: dayjs('2023-05-09T10:00:00.000Z').format(
+      'DD/MM/YYYY - HH:mm'
+    ),
   },
 ];
 
